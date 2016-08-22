c785219fd5effea27dc81930c58f3a81fc015c84
Charles Hines <cdh918@gmail.com>
2016-08-22 16:16:26 -0400

    Initial commit

---
+++ src/main/java/hines/cd/fileviewer/App.java
@@ -0,0 +1,13 @@
+package hines.cd.fileviewer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
